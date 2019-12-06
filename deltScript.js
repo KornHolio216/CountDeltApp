001417140ae97bc6659eba49740318907a9786b2
KornHolio216 <machs1337@gmail.com>
2019-12-06 23:02:46 +0100

    css btn and input changes, js getting elements

--- deltScript.js
+++ deltScript.js
@@ -56,10 +56,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const delta = Math.pow(b,2)-4*a*c
     console.log(delta);
     const x1 = (-b-Math.sqrt(delta))/2*a;
-   
+    console.log(x1);
     const x2 = (-b+Math.sqrt(delta))/2*a;
-   
-    document.getElementById("resultX1").textContent =`x1: ${x1}`
+    console.log(x2)
+    document.getElementById("resultX1").textContent = `x1: ${x1}`
     document.getElementById("resultX2").textContent = `x2: ${x2}`
     document.getElementById("resultDelt").textContent = ` Δ = ${delta}`
     };
