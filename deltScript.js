254af7b90740f13de1f8fed377dc65ffb7016c13
KornHolio216 <machs1337@gmail.com>
2019-12-09 19:52:41 +0100

    two decimals

--- deltScript.js
+++ deltScript.js
@@ -56,13 +56,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const delta = Math.pow(b,2)-4*a*c   
     document.getElementById("resultDelt").textContent = `${delta}`
     const deltRoot = Math.sqrt(delta)
-    document.getElementById("resultRoot").textContent = `${deltRoot}`
+    document.getElementById("resultRoot").textContent = `${deltRoot.toFixed(2)}`
 
     if (delta>0){
         const x1 = (-b-Math.sqrt(delta))/2*a;
         const x2 = (-b+Math.sqrt(delta))/2*a;
-        document.getElementById("resultX1").textContent = `${x1}`
-        document.getElementById("resultX2").textContent = `${x2}` 
+        document.getElementById("resultX1").textContent = `${x1.toFixed(2)}`
+        document.getElementById("resultX2").textContent = `${x2.toFixed(2)}` 
         document.getElementById("resultX0").textContent = "-"
     } else if(delta===0){
         const x0 = (-b)/2*a;
