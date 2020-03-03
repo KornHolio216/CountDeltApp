2e40b3e421aed45df56cce4dc42cbdf5ab405098
KornHolio216 <machs1337@gmail.com>
2020-03-03 18:30:42 +0100

    nawet nie wiem czemu to commituje XD

---
+++ src/js/toggle.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector(".NavbarToggle")
+const navbar = document.querySelector(".NavBar")
+
+toggle.addEventListener(onclick, toggleNav)
+
+function toggleNav(){
+   return  console.log("fasf")
+}
+// co to jest za gowno XDDDDD
