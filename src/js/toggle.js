1b204c3e25de14aa662fc17936c46cd582666a4e
KornHolio216 <machs1337@gmail.com>
2020-03-08 14:11:56 +0100

    hamburger functionality

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,38 +1,8 @@
-const toggle = document.querySelector(".NavbarToggle").addEventListener("click", navDisplay())
-
-function navDisplay(){
-   const navbar = document.querySelector(".Navbar")
-   navbar.style.display = "none"
-}
-
-/* function changeNavDisplay() {
-   let navbar = document.querySelector(".Navbar")
-   navbar.classList.toggle("visible")
-   console.log("chuj")
-}
-
 function toggle(){
-   const toggle = document.querySelector(".NavbarToggle")
-   toggle.addEventListener("click", changeNavDisplay, false)
-}
-
-const tl = new TimelineLite({paused : true, reversed: true})
-
-tl.to("navOpen", 1, {
-   width: "100%",
-   ease: Power2.easeOut
-})
-
-
-navButton.addEventListener("click", (e) =>{
-   if(tl.isActive()){
-      e.preventDefault()
-      e.stopImmediatePropagation()
-      return false
+   var links = document.getElementById("myLinks")
+   if (links.style.display === "flex"){
+      links.style.display = "none"
+   } else {
+      links.style.display = "flex"
    }
-toggleTween(tl)
-})
-
-function toggleTween(tween){
-   tween.reversed() ? tween.play() : tween.reverse()
-}  */
+}
