26e2e3393158bd6295c552bb6969db7878cbcbad
KornHolio216 <machs1337@gmail.com>
2020-03-14 17:45:26 +0100

    hamburger menu

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,8 +1,13 @@
-function toggle(){
-   var links = document.getElementById("myLinks")
-   if (links.style.display === "flex"){
-      links.style.display = "none"
-   } else {
-      links.style.display = "flex"
-   }
-}
+const closeBtn = document.querySelector(".closebtn")
+const toggleBtn = document.querySelector(".menu-icon")
+const navbar = document.querySelector("#navbar")
+
+function closeMenu(){
+    navbar.style.height = "0vh";
+  }
+  function openMenu(){
+    navbar.style.height = "100vh";
+  }
+
+toggleBtn.addEventListener("click", openMenu)
+closeBtn.addEventListener("click", closeMenu)
